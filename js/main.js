0e388b1e9ab920e4dcd01fc01e875eb4a9ef5c71
Alexis Demaria <dademaria10@gmail.com>
2022-07-12 17:01:35 -0300

    SegundaEntrega

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-let limSup = parseInt(prompt('Ingresar cantidad de números que quiere sumar'))
+let limSup = parseInt(prompt('Ingrese cuantas personas quiere registrar'))
+if (limSup > 0){
+    let suma = 0
+    let cantPersona = 0
+    // let edad = 0
+    for(let  i = 1; i <= limSup; i++){
+        let edad = parseInt(prompt('Ingrese edad de la persona'))
+        suma += edad
+        cantPersona += 1
 
-let suma = 0
-for(let  i = 1; i <= limSup; i++){
-    let num = parseInt(prompt('Ingrese el numero a sumar'))
-    suma += num
-
+    }
+    let prom = suma / cantPersona
+    console.log(prom)
+    alert('El promedio de edad es:  ' + prom)
+}else{
+    alert('No se puede calcular el promedio de edad de 0 personas')
 }
-
-console.log(suma)
-alert('La suma total es: ' + suma)
