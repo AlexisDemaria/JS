2297ba33d2dcf9c1fb717badb8525f365c7dd370
Alexis Demaria <dademaria10@gmail.com>
2022-07-13 00:13:15 -0300

    TerceraEntrega

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-let limSup = parseInt(prompt('Ingrese cuantas personas quiere registrar'))
-if (limSup > 0){
-    let suma = 0
-    let cantPersona = 0
-    // let edad = 0
-    for(let  i = 1; i <= limSup; i++){
-        let edad = parseInt(prompt('Ingrese edad de la persona'))
-        suma += edad
-        cantPersona += 1
-
-    }
-    let prom = suma / cantPersona
-    console.log(prom)
-    alert('El promedio de edad es:  ' + prom)
+// Inicializo arreglos.
+const nombres = []
+const edades = []
+// Ingresar cantidad de nombres a cargar, si se ingresa 0 o menor, se detiene la carga.
+let cantNombres = parseInt(prompt('Ingresar cantidad de nombres que quiera cargar'))
+if (cantNombres <= 0){
+    alert('No puede ingresar 0 nombres')
+    
 }else{
-    alert('No se puede calcular el promedio de edad de 0 personas')
+    // Ingresar nombres y agregarlos al arreglo nombres.
+    do{
+        let ingresarNombre = prompt("Ingresar nombre")
+        nombres.push(ingresarNombre)
+
+    }while(nombres.length != cantNombres)
+
+    // Ingresar edades y agregarlos al arreglo edad.
+    do{
+        let ingresarEdad = prompt("Ingresar edad")
+        edades.push(ingresarEdad)
+
+    }while(edades.length != cantNombres)
+
+    // Mostrar los nombres y edades ingresados.
+    alert('Nombres:' + ("\n") + ("\n") + nombres.join("\n") + ("\n") + ("\n") + 'Edades:' + ("\n") + ("\n") + edades.join("\n"))
+
 }
